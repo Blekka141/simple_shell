b25362c928fbfe9c1ec116b1843cc8e1b014de6b
Blekka141 <144395417+Blekka141@users.noreply.github.com>
2023-12-13 14:51:49 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,45 +1,49 @@
 #include "shell.h"
 /**
- * execute - execute a command with its entire path variables.
- * @data: a pointer to the program's data
- * Return: If sucess returns zero, otherwise, return -1.
+ * execute - Execute a command with its entire path variables.
+ * @data: Pointer to the program's data structure.
+ * Return: If successful, returns zero otherwise, return -1.
  */
 int execute(data_of_program *data)
 {
 	int retval = 0, status;
 	pid_t pidd;
 
-	/* check for program in built ins */
+	/* Check if the program is a built-in command */
 	retval = builtins_list(data);
-	if (retval != -1)/* if program was found in built ins */
+	if (retval != -1) /* If the program was found in built ins */
 		return (retval);
 
-	/* check for program file system */
+	/* Check for the program in the file system */
 	retval = find_program(data);
 	if (retval)
-	{/* if program not found */
+	{/* If the program was not found, return an error code */
 		return (retval);
 	}
 	else
-	{/* if program was found */
-		pidd = fork(); /* create a child process */
+	{/* If the program was found, create a child process */
+		pidd = fork();
 		if (pidd == -1)
-		{ /* if the fork call failed */
+		{ 
+			/* If the fork call failed, print an error and exit */
 			perror(data->command_name);
 			exit(EXIT_FAILURE);
 		}
 		if (pidd == 0)
-		{/* I am the child process, I execute the program*/
+		{/* Child process, execute the program */
 			retval = execve(data->tokens[0], data->tokens, data->env);
-			if (retval == -1) /* if error when execve*/
+			if (retval == -1) 
+			/* If there was an error when execve, print an error and exit */
 				perror(data->command_name), exit(EXIT_FAILURE);
 		}
 		else
-		{/* I am the father, I wait and check the exit status of the child */
+		{/* Parent process, wait for the child and check its exit status */
 			wait(&status);
 			if (WIFEXITED(status))
+			/* If the child process exited normally, set errno to its exit status */
 				errno = WEXITSTATUS(status);
 			else if (WIFSIGNALED(status))
+			/* If the child process was terminated by a signal set errno accordingly */
 				errno = 128 + WTERMSIG(status);
 		}
 	}
