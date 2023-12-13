c5c34df8118e614eca1cd72130baffb2cc2d6866
Blekka141 <144395417+Blekka141@users.noreply.github.com>
2023-12-13 13:19:12 +0200

    Update builtins_env.c

--- builtins_env.c
+++ builtins_env.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * builtin_env - shows the environment where the shell runs
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_env - Displays the shell environment.
+ * @data: Pointer to the program's data structure.
+ * Return: Returns zero on success or other values for specific error conditions.
  */
 int builtin_env(data_of_program *data)
 {
@@ -11,29 +11,31 @@ int builtin_env(data_of_program *data)
 	char cpname[50] = {'\0'};
 	char *var_copy = NULL;
 
-	/* if not arguments */
+	/* Check if there are no arguments */
 	if (data->tokens[1] == NULL)
 		print_environ(data);
 	else
 	{
 		for (i = 0; data->tokens[1][i]; i++)
-		{/* checks if exists a char = */
+		{
+			/* Checks if there is an '=' characterin the argument */
 			if (data->tokens[1][i] == '=')
-			{/* checks if exists a var with the same name and change its value*/
-			/* temporally */
+			{
+				/* Temporarily copy and modify an environmental variable if it exists */
 				var_copy = str_duplicate(env_get_key(cpname, data));
 				if (var_copy != NULL)
 					env_set_key(cpname, data->tokens[1] + i + 1, data);
 
-				/* print the environ */
+				/* Print the environment */
 				print_environ(data);
 				if (env_get_key(cpname, data) == NULL)
-				{/* print the variable if it does not exist in the environ */
+				{/* Print the variable if it does not exist in the environment */
 					_print(data->tokens[1]);
 					_print("\n");
 				}
 				else
-				{/* returns the old value of the var*/
+				{
+					/* Restore the old value of the variable */
 					env_set_key(cpname, var_copy, data);
 					free(var_copy);
 				}
@@ -49,13 +51,13 @@ int builtin_env(data_of_program *data)
 }
 
 /**
- * builtin_set_env - ..
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_set_env - Sets or modifies an environment variable.
+ * @data: Pointer to the program's data structure.
+ * Return: Returns zero on success, or other values for specific error conditions. 
  */
 int builtin_set_env(data_of_program *data)
 {
-	/* validate args */
+	/* Validate arguments */
 	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
 		return (0);
 	if (data->tokens[3] != NULL)
@@ -64,20 +66,21 @@ int builtin_set_env(data_of_program *data)
 		perror(data->command_name);
 		return (5);
 	}
-
+	/* Set or modify the environment variable */
 	env_set_key(data->tokens[1], data->tokens[2], data);
 
 	return (0);
 }
 
 /**
- * builtin_unset_env - ..
- * @data: struct for the program's data'
- * Return: ..
+ * builtin_unset_env - Remove an environment variable.
+ * @data: Pointer to the program's data structure.
+ * Return: Returns zero on success or other values for specific error conditions.
  */
+
 int builtin_unset_env(data_of_program *data)
 {
-	/* validate args */
+	/* Validate arguments */
 	if (data->tokens[1] == NULL)
 		return (0);
 	if (data->tokens[2] != NULL)
@@ -86,6 +89,7 @@ int builtin_unset_env(data_of_program *data)
 		perror(data->command_name);
 		return (5);
 	}
+	/* Remove the environment variable */
 	env_remove_key(data->tokens[1], data);
 
 	return (0);
